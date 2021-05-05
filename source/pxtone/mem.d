6428a90221dc65e1aad7f1cecb21870d9d30f32b
Cameron Ross <elpenguino@gmail.com>
2021-05-04 22:34:36 -0300

    use std.experimental.allocator instead of malloc/free

--- source/pxtone/mem.d
+++ source/pxtone/mem.d
@@ -1,44 +1,18 @@
 ﻿module pxtone.mem;
 
-import core.stdc.stdlib;
-
-bool pxtnMem_zero(void* p, size_t byte_size) nothrow @system {
-	(cast(ubyte*) p)[0 .. byte_size] = 0;
-	return true;
-}
-
-void* allocate(size_t size) nothrow @system {
-	return malloc(size);
-}
+import std.experimental.allocator;
 
 T* allocate(T)() {
-	import std.conv : emplace;
-
-	auto result = cast(T*) malloc(T.sizeof);
-	emplace(result);
-	return result;
+	return theAllocator.make!T;
 }
-T[] allocate(T)(size_t count) {
-	import std.conv : emplace;
 
-	auto result = (cast(T*) malloc(T.sizeof * count))[0 .. count];
-	foreach (idx, _; result) {
-		emplace(&result[idx]);
-	}
-	return result;
+T[] allocate(T)(size_t count) {
+	return theAllocator.makeArray!T(count);
 }
 
 void deallocate(T)(ref T[] array) nothrow @system {
-	if (!array) {
-		return;
-	}
-	free(array.ptr);
-	array = null;
+	return theAllocator.dispose(array);
 }
 void deallocate(T)(ref T* ptr) nothrow @system {
-	if (!ptr) {
-		return;
-	}
-	free(ptr);
-	ptr = null;
+	return theAllocator.dispose(ptr);
 }
